6943fdf7411e3abf0fb07867a7e534304e560db3
Matthijs Kooijman <matthijs@stdin.nl>
2023-10-20 01:07:17 +0200

    Using gettext_lazy instead of ugettext_lazy
    The latter is just an alias of the former and was deprecated in Django
    3.0 and removed in Django 4.0.

--- konst/tests/models.py
+++ konst/tests/models.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from konst import Constant, ConstantGroup, Constants
 from konst.models.fields import ConstantChoiceCharField, ConstantChoiceField
